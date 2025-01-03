05cc76c8387abef55e23da13110ae1be7f45befa
lawrenceku <lawrencekunmilola@gmail.com>
2025-01-03 19:45:00 +0100

    Connect to render postgresql db instance

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,13 +1,21 @@
-module.exports = {
-    HOST: "localhost",
-    USER: "postgres",
-    PASSWORD: "password",
-    DB: "postgres",
-    dialect: "postgres",
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
-    }
-  };
+const config = {
+  HOST: process.env.DB_HOST || "localhost",
+  USER: process.env.DB_USER || "postgres",
+  PASSWORD: process.env.DB_PASSWORD || "password",
+  DB: process.env.DB_NAME || "postgres",
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // For Render's self-signed certificates
+    },
+  },
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
+};
+
+module.exports = config;
