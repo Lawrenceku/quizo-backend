55257295d460b3cdb882acbe896af069dfd9ce36
Lawrence <lawrencekunmilola@gmail.com>
2025-01-09 21:47:38 +0100

    fix: routing controller issues

--- src/routes/quiz.js
+++ src/routes/quiz.js
@@ -2,6 +2,6 @@ const express = require('express');
 const quizController = require('../controllers/quiz.controller');
 const router = express.Router();
 
-router.get('/questions', quizController.getAllQuestions);
+router.get('/questions', quizController.getRandomQuestions);
 
 module.exports = router;
